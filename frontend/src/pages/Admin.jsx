57736e0c937380013dbca6fc36f086efa831f4b0
fwedwicc <moreno.frederick.capiral@gmail.com>
2025-04-13 19:37:07 +0800

    Update page title and enhance Admin component with animations and custom scrollbar styling

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
+import { motion } from 'framer-motion'
 import { Link, useLocation } from 'react-router-dom'
 import Swal from 'sweetalert2'
 import { Contact, FAQs, Article, Dashboard } from '../components/admin'
+import { HiLogout, HiOutlineMail, HiOutlineViewGrid, HiOutlineHome, HiOutlineBookmarkAlt, HiOutlineInformationCircle, HiOutlineChevronDoubleRight } from "react-icons/hi"
 import { Button } from '../components/ui'
 
 const Admin = () => {
@@ -57,93 +59,73 @@ const Admin = () => {
   const ButtonLink = ({ link, icon, label, isNavOpen }) => {
     return (
       <Link to={link} className='relative group'>
-        <Button className={`justify-start w-full ease-in-out transition-all duration-300 gap-3 ${location.pathname === link ? 'border bg-neutral-50/50 text-neutral-600' : 'border-none text-neutral-700'} ${!isNavOpen ? 'size-11.5 rounded-xl' : 'rounded-lg'}`}>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="shrink-0 size-5">
-            <path strokeLinecap="round" strokeLinejoin="round" d={icon} />
-          </svg>
+        <Button className={`justify-start w-full ease-in-out transition-all duration-300 gap-3 ${location.pathname === link ? 'border border-neutral-200 bg-neutral-100/80 text-neutral-600' : 'border-none text-neutral-700'} ${!isNavOpen ? 'size-11.5 rounded-xl' : 'rounded-lg'}`}>
+          <span>{icon}</span>
           {isNavOpen && <span>{label}</span>}
         </Button>
-        {!isNavOpen && <span className='border bg-white rounded-md text-sm px-1.5 py-1 absolute opacity-0 group-hover:opacity-100 transform -translate-y-1/2 top-1/2 left-13 transition-opacity duration-200 ease-in-out'>{label}</span>}
+        {!isNavOpen && <span className='border border-neutral-200 bg-white rounded-lg text-sm px-2 py-1 absolute opacity-0 group-hover:opacity-100 transform -translate-y-1/2 top-1/2 left-13 transition-opacity duration-200 ease-in-out pointer-events-none'>{label}</span>}
       </Link>
     )
   }
 
   return (
-    <>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
       {/* Side Nav */}
       <nav className={`lg:block hidden fixed p-3 h-screen ${isNavOpen ? 'w-56' : 'w-18'} transition-all duration-300`}>
         <div className='relative h-full flex flex-col justify-between'>
           <div>
             {/* Logo */}
             <div className='flex items-center gap-3'>
-              <div className='flex items-center justify-center size-12 shrink-0 bg-neutral-600 text-white border-4 border-neutral-200 rounded-2xl'>
-                x
+              <div className='relative flex justify-center items-center shrink-0 size-12 bg-neutral-800 border rounded-xl'>
+                <span className='text-white font-bold'>S</span>
               </div>
-              <span className={`${isNavOpen ? 'block' : 'hidden'} transition-all duration-300`}>Bhieee</span>
+              <h5 className={`${isNavOpen ? 'block' : 'hidden'} transition-all duration-300`}>StaySuite</h5>
             </div>
-            <Button onClick={handleNavToggle} className={'absolute top-0 -right-9 size-7'}>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`shrink-0 size-3 ${isNavOpen ? 'rotate-180' : ''}`}>
-                <path strokeLinecap="round" strokeLinejoin="round" d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5" />
-              </svg>
+            <Button onClick={handleNavToggle} className={'absolute top-3 -right-12 size-7 bg-neutral-50 border border-neutral-100'}>
+              <HiOutlineChevronDoubleRight className={`size-3 shrink-0 transition-all duration-300 ease-in-out ${isNavOpen ? 'rotate-180' : ''}`} />
             </Button>
             <ul className="mt-7 flex flex-col gap-1">
-              <ButtonLink
-                link='/'
-                icon='m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25'
-                label='Home'
-                isNavOpen={isNavOpen}
-              >
-              </ButtonLink>
-              <ButtonLink
-                link='/admin'
-                icon='M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z'
-                label='Dashboard'
-                isNavOpen={isNavOpen}
-              >
-              </ButtonLink>
-              <ButtonLink
-                link='/admin/contact'
-                icon='M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75'
-                label='Contact'
-                isNavOpen={isNavOpen}
-              >
-              </ButtonLink>
-              <ButtonLink
-                link='/admin/faqs'
-                icon='M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z'
-                label='FAQs'
-                isNavOpen={isNavOpen}
-              >
-              </ButtonLink>
-              <ButtonLink
-                link='/admin/article'
-                icon='M16.5 3.75V16.5L12 14.25 7.5 16.5V3.75m9 0H18A2.25 2.25 0 0 1 20.25 6v12A2.25 2.25 0 0 1 18 20.25H6A2.25 2.25 0 0 1 3.75 18V6A2.25 2.25 0 0 1 6 3.75h1.5m9 0h-9'
-                label='Article'
-                isNavOpen={isNavOpen}
-              >
-              </ButtonLink>
+              {[
+                { 'label': 'Home', 'link': '/', 'icon': HiOutlineHome },
+                { 'label': 'Dashboard', 'link': '/admin', 'icon': HiOutlineViewGrid },
+                { 'label': 'Contact', 'link': '/admin/contact', 'icon': HiOutlineMail },
+                { 'label': 'FAQs', 'link': '/admin/faqs', 'icon': HiOutlineInformationCircle },
+                { 'label': 'Article', 'link': '/admin/article', 'icon': HiOutlineBookmarkAlt }
+              ].map((item, index) => (
+                <ButtonLink
+                  key={index}
+                  link={item.link}
+                  icon={<item.icon className='size-6 stroke-[1.3px]' />}
+                  label={item.label}
+                  isNavOpen={isNavOpen}
+                >
+                </ButtonLink>
+              ))}
             </ul>
           </div>
           <div className='relative group'>
-            <Button onClick={handleLogout} className={`w-full text-red-500 border-black ease-in-out transition duration-300 ${!isNavOpen ? 'size-11.5' : null}`}>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
-              </svg>
+            <Button onClick={handleLogout} variant='danger' className={`w-full ${!isNavOpen ? 'size-11.5' : null}`}>
+              <HiLogout className='size-5' />
               {isNavOpen && 'Logout'}
             </Button>
-            {!isNavOpen && <span className='border bg-white rounded-md text-sm px-2 py-1 absolute opacity-0 group-hover:opacity-100 transform -translate-y-1/2 top-1/2 left-13 transition-opacity duration-200 ease-in-out'>Logout</span>}
+            {!isNavOpen && <span className='border border-neutral-200 bg-white rounded-lg text-sm px-2 py-1 absolute opacity-0 group-hover:opacity-100 transform -translate-y-1/2 top-1/2 left-13 transition-opacity duration-200 ease-in-out pointer-events-none'>Logout</span>}
           </div>
         </div>
       </nav>
       {/* Content */}
-      <main className={`w-full bg-stone-50/50 pl-0 ${isNavOpen ? 'lg:pl-56' : 'lg:pl-18'} transition-all duration-300`}>
-        <section className='py-3.5 pr-3.5 lg:pl-0 pl-3.5 bg-stone-50/50 h-screen'>
-          <div className='bg-white border border-stone-200/50 rounded-2xl overflow-auto lg:h-full h-[93%] lg:mt-0 mt-12 p-12'>
+      <main className={`w-full bg-neutral-50/40 pl-0 ${isNavOpen ? 'lg:pl-56' : 'lg:pl-18'} transition-all duration-300`}>
+        <section className='py-3.5 pr-3.5 lg:pl-0 pl-3.5 bg-neutral-50/40 h-screen'>
+          <div className='bg-white border border-neutral-200/60 rounded-2xl overflow-auto custom-scollbar lg:h-full h-[93%] lg:mt-0 mt-12 p-12'>
             {renderComponent()}
           </div>
         </section>
       </main>
-    </>
+    </motion.div>
   )
 }
 
